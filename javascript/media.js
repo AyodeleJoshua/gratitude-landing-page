a2c777db8c9ffbef1e1cb3c9dd8f2d3309eccffe
Joshua Ayodele <jayodele64@gmail.com>
2021-10-02 15:11:28 +0100

    initial commit

---
+++ javascript/media.js
@@ -0,0 +1,33 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach((tab) => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.tabTarget);
+		tabContents.forEach((tabContent) => {
+			tabContent.classList.remove('active');
+		});
+		tabs.forEach((tab) => {
+			tab.classList.remove('active');
+		});
+		tab.classList.add('active');
+		target.classList.add('active');
+        
+	});
+});
+const india = document.querySelector('.nav-ribbon-first-child-child');
+const dropDown = document.querySelector('.drop-down');
+
+let isDropDownOpen = false;
+
+india.addEventListener('click', () => {
+	if (isDropDownOpen) {
+
+		dropDown.style.border = '1px solid #d1d1d1';
+		dropDown.style.height = '7em';
+	} else {
+		dropDown.style.border = '0';
+		dropDown.style.height = '0';
+	}
+	isDropDownOpen = !isDropDownOpen;
+});
---
+++ javascript/script-philippine.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+	$('.slider').bxSlider({
+		mode: 'fade',
+		captions: true,
+		pager: false,
+	});
+});
+
+$(document).ready(function () {
+	jQuery('.bxslider-carousel-1').bxSlider({
+		minSlides: 1,
+		maxSlides: 8,
+		slideWidth: 150,
+		slideMargin: 10,
+		ticker: true,
+		speed: 50000,
+	});
+});
+$(document).ready(function () {
+	jQuery('.bxslider-carousel-2').bxSlider({
+		minSlides: 1,
+		maxSlides: 8,
+		slideWidth: 150,
+		slideMargin: 10,
+		ticker: true,
+		speed: 50000,
+	});
+});
+
+$(window).scroll(function () {
+	var position = $(window).scrollTop();
+	var pos2 = Math.floor($('#counter').offset().top - 200);
+
+	if (Math.floor(position) >= pos2) {
+		$('.count').each(function () {
+			$(this)
+				.prop('Counter', 0)
+				.animate(
+					{
+						Counter: $(this).data('value'),
+					},
+					{
+						duration: 4000,
+						easing: 'swing',
+						step: function (now) {
+							$(this).text(Math.ceil(now));
+						},
+					}
+				);
+		});
+	}
+});
+const india = document.querySelector('.nav-ribbon-first-child-child');
+const dropDown = document.querySelector('.drop-down');
+
+let isDropDownOpen = false;
+
+india.addEventListener('click', () => {
+	if (isDropDownOpen) {
+
+		dropDown.style.border = '1px solid #d1d1d1';
+		dropDown.style.height = '7em';
+	} else {
+		dropDown.style.border = '0';
+		dropDown.style.height = '0';
+	}
+	isDropDownOpen = !isDropDownOpen;
+});
